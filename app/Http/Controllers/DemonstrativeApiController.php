e51096c74691ed4098ab3bbea63e0f95cc4714b1
Antonio Jose [UpDEV] <falecomoantonio@live.com>
2024-01-23 10:58:47 -0300

    Adicionado controle de visão para os blocos de forma de pagamento.

--- app/Http/Controllers/DemonstrativeApiController.php
+++ app/Http/Controllers/DemonstrativeApiController.php
@@ -72,42 +72,44 @@ class DemonstrativeApiController extends Controller
        }
     }
 
-    public function register($id, Request $request)
+    public function register($pathUuid, $id, Request $request)
     {
         try
         {
             $uuid = decrypt($id);
-            $dataJson = json_encode($request->all());
+            if ($uuid == $pathUuid) {
+                $dataJson = json_encode($request->all());
 
-            $enterprise = Enterprise::query()->where('uuid','=', $uuid)->firstOrFail();
+                $enterprise = Enterprise::query()->where('uuid', '=', $uuid)->firstOrFail();
 
-            if ($enterprise == null) {
-                throw new \Exception("Empresa não foi localizada.");
+                if ($enterprise == null) {
+                    throw new \Exception("Empresa não foi localizada.");
+                }
+
+                if ($enterprise->user->id_status == UserStatusEnum::SUSPENDED) {
+                    throw new \Exception("Conta suspensa, por favor, entre em contato com o suporte técnico.");
+                }
+
+                $demostrative = Demostrative::query()->where('enterprise_id', '=', $enterprise->id)->first();
+                if ($demostrative == null) {
+                    $dataToSave = [
+                        'payload' => $dataJson,
+                        'enterprise_id' => $enterprise->id
+                    ];
+
+                    $demostrative = new Demostrative($dataToSave);
+                    $demostrative->save();
+                } else {
+                    $demostrative->payload = $dataJson;
+                    $demostrative->updated_at = Carbon::now();
+                    $demostrative->update();
+                }
+
+                return response(['status' => 'Atualizado', 'return' => 200], 200);
             }
-
-            if ($enterprise->user->id_status == UserStatusEnum::SUSPENDED) {
-                throw new \Exception("Conta suspensa, por favor, entre em contato com o suporte técnico.");
+            else {
+                return response(['status' => 'Atualizado', 'return' => 404], 200);
             }
-
-            $demostrative = Demostrative::query()->where('enterprise_id','=', $enterprise->id)->first();
-            if ($demostrative == null)
-            {
-                $dataToSave = [
-                    'payload' => $dataJson,
-                    'enterprise_id' => $enterprise->id
-                ];
-
-                $demostrative = new Demostrative($dataToSave);
-                $demostrative->save();
-            }
-            else
-            {
-                $demostrative->payload = $dataJson;
-                $demostrative->updated_at = Carbon::now();
-                $demostrative->update();
-            }
-
-            return response(['status' => 'Atualizado'], 200);
         }
         catch (\Exception $e)
         {
@@ -115,7 +117,6 @@ class DemonstrativeApiController extends Controller
             logger($e);
             logger($id);
             logger($request->all());
-
             return response(['status' =>'ERROR', 'Message' => $e->getMessage() ,'return' => $e->getCode()], 400);
         }
     }
